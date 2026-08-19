260e7e47f5ed3124ec200c1234e8a53d3f89bae6
itsmohithere <itsmohitsaxenahere@gmail.com>
2026-08-19 19:47:50 +0530

    test CI

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
         </header>
 
         <section className="space-y-6">
+          <img src="" />
           <div className="space-y-2">
             <h2 className="text-xl font-semibold text-text-primary">Button</h2>
 
