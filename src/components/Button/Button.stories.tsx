b360dd8b6df84a7f0d9c12ee9c0927c58c71922f
itsmohithere <itsmohitsaxenahere@gmail.com>
2026-08-19 20:05:43 +0530

    separate storybook job

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { Button } from './Button';
+import { Button } from './Button/as';
 
 const meta = {
   title: 'Components/Button',
